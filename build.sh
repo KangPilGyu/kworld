b93e7213e38e97843af39c203be770cdc13310ee
eiffe <eiffe@DESKTOP-2MLVAB8>
2018-12-14 20:22:42 +0900

    Deploy-Test

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-﻿#!/bin/bash
+﻿# encoding: utf-8  
+#!/bin/bash
 sudo mv -f /home/ubuntu/app/travis/build/target/ROOT.war /var/lib/tomcat8/webapps
+echo "Tomcat Restart"
 sudo service tomcat8 restart
+echo "Tomcat Starting"
